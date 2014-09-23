b28585ec9eb11ab1cee4fd2f2d3df3181435a175
Jim Hester <james.f.hester@gmail.com>
2014-09-23 16:46:24 -0400

    Export modifiers and re-document

--- R/modifiers.R
+++ R/modifiers.R
@@ -1,23 +1,29 @@
+#' @export
 zero_or_more <- function(...) {
   p("(?:", ..., ")*")
 }
 
+#' @export
 one_or_more <- function(...) {
   p("(?:", ..., ")+")
 }
 
+#' @export
 maybe <- zero_or_one <- function(...) {
   p("(?:", ..., ")?")
 }
 
+#' @export
 possessive <- function(...) {
   p("(?:", ..., ")*+")
 }
 
+#' @export
 lazy <- first_match <- match_once <- function(...) {
   p("(?:", ..., ")*?")
 }
 
+#' @export
 not <- function(...) {
   p("[^", ..., "]")
 }

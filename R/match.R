a233cab4269c83a8573bef1e15b29a6bed55d450
Jim Hester <james.f.hester@gmail.com>
2014-09-25 14:11:47 -0400

    Formatting changes to conform with http://r-pkgs.had.co.nz/style.html

--- R/match.R
+++ R/match.R
@@ -8,19 +8,19 @@
 #' there are captures in the regular expression, returns a list of named
 #' character vectors with the captured text.  If the g option is used with
 #' capturing, the output is a list of lists.
-#' @seealso \code{\link{regex}} Section 'Perl-like Regular Expressions' for a
+#' @seealso \code{\link{regex}} Section `Perl-like Regular Expressions` for a
 #' discussion of the supported options
 #' @export
-match = function(data, pattern, options=""){
-  process_matches <- function(res, data){
-    starts <- attr(res, 'capture.start')
-    if(is.null(starts)){
+re_matches <- function(data, pattern, options = "") {
+  process_matches <- function(res, data) {
+    starts <- attr(res, "capture.start")
+    if (is.null(starts)) {
       return(res != -1)
     }
-    lengths <- attr(res, 'capture.length')
-    names <- attr(res, 'capture.names')
+    lengths <- attr(res, "capture.length")
+    names <- attr(res, "capture.names")
     ret = list()
-    for(itr in seq_len(ncol(starts))){
+    for (itr in seq_len(ncol(starts))) {
       ret[[itr]] = unname(ifelse(starts[,itr] == -1, "FALSE",
                           substring(data, starts[,itr], starts[,itr] + lengths[,itr] - 1)))
     }
@@ -28,14 +28,14 @@ match = function(data, pattern, options=""){
     ret
   }
 
-  if(grepl('g', options)){
-    options <- gsub('g', '', options)
+  if (grepl("g", options)) {
+    options <- gsub("g", "", options)
     pattern <- add_options(pattern, options)
-    mapply(process_matches, gregexpr(pattern=pattern, data, perl=T), data)
+    mapply(process_matches, gregexpr(pattern = pattern, data, perl = TRUE), data)
   }
-  else{
+  else {
     pattern <- add_options(pattern, options)
-    process_matches(regexpr(pattern=pattern, data, perl=T), data)
+    process_matches(regexpr(pattern = pattern, data, perl = TRUE), data)
   }
 }
 
@@ -45,26 +45,26 @@ match = function(data, pattern, options=""){
 #' @param pattern regular expression to match
 #' @param replacement replacement text to use
 #' @param options option flags
-#' @seealso \code{\link{regex}} Section 'Perl-like Regular Expressions' for a
+#' @seealso \code{\link{regex}} Section "Perl-like Regular Expressions" for a
 #' discussion of the supported options
 #' @examples
-#' string = c('this is a Test', 'string')
-#' s(string, 'test', 'not a test', 'i')
-#' s(string, 'i', 'x', 'g')
+#' string = c("this is a Test", "string")
+#' s(string, "test", "not a test", "i")
+#' s(string, "i", "x", "g")
 #' @export
-substitute <- function(data, pattern, replacement, options='') {
-  res <- if(grepl(options, 'g') == TRUE){
-    options <- gsub('g', '', options)
+re_substitutes <- function(data, pattern, replacement, options = "") {
+  res <- if (grepl(options, "g") == TRUE) {
+    options <- gsub("g", "", options)
     pattern <- add_options(pattern, options)
-    gsub(x=data, pattern=pattern, replacement=replacement, perl=T)
+    gsub(x = data, pattern = pattern, replacement = replacement, perl = TRUE)
   }
-  else{
+  else {
     pattern <- add_options(pattern, options)
-    sub(x=data, pattern=pattern, replacement=replacement, perl=T)
+    sub(x = data, pattern = pattern, replacement = replacement, perl = TRUE)
   }
   res
 }
 
-add_options <- function(pattern, options){
+add_options <- function(pattern, options) {
   p("(?", p(options), ")", pattern)
 }

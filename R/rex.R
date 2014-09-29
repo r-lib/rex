d4ad484350072525295a4623a4be69357254c24d
Jim Hester <james.f.hester@gmail.com>
2014-09-28 21:26:50 -0400

    Document functions
    - capture
    - group

--- R/rex.R
+++ R/rex.R
@@ -2,25 +2,65 @@
 #' @include utils.R
 NULL
 
+#' Create a capture group
+#'
+#' Used to save the matched value within the group for use later in the regular
+#' expression or to extract the values captured.  Types of capture groups.
+#' \describe{
+#'   \item{Unnamed Capture}{- \code{\link{capture}} or
+#'   \code{\link{.}}.  The groups are numbered incrementally starting at one.}
+#'   \item{Named Capture}{- \code{\link{named_capture}} or
+#'   \code{\link{..}}.  The groups are named explicitly by the user.}
+#' }
+#'
+#' @param name of the group.  Unnamed capture groups are numbers starting at 1
+#' in the order they appear in the regular expression.  If two groups have the
+#' same name, the leftmost group is the used in any reference.
+#' @param ... variables to capture.  Can be \code{\link{shortcuts}}, R
+#' variables, text, or other \pkg{rex} functions.
 #' @export
 #' @family rex
+#' @rdname capture
+#' @aliases .
+#' @seealso \code{\link{group}} for grouping without capturing.  Perl 5 capture
+#' groups \url{http://perldoc.perl.org/perlre.html#Capture-groups}
+#' @examples
+#'
+#' # Match paired quotation marks
+#' re <- rex(
+#'   # first quotation mark
+#'   capture(quotes),
+#'
+#'   # match all non-matching quotation marks
+#'   zero_or_more(except(capture_group(1))),
+#'
+#'   # end quotation mark (matches first)
+#'   capture_group(1)
+#' )
 capture <- . <- function(...) {
   p( "(", p(escape_dots(...)), ")" )
 }
 
 #' @export
-#' @family rex
+#' @rdname capture
+#' @aliases ..
 named_capture <- .. <- function(name, ... ) {
   p( "(?<", name, ">", p(escape_dots(...)), ")" )
 }
 
 #' @export
-#' @family rex
+#' @rdname capture
 capture_group <- function(name) {
-  # \g{something} syntax supports both digits and named groups
+  # \g{something}
   p( "\\g{", name, "}" )
 }
 
+#' Create a grouped expression
+#'
+#' This is similar to \code{\link{capture}} except that it does not store the
+#' value of the group.  This can be used when you want to combine several parts
+#' together but do not need to later reference the grouped value.
+#' @inheritParams capture
 #' @export
 #' @family rex
 group <- function(...) {

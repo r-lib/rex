ee5cf374391531c183f555f3cbe6190e780ccf15
Kevin Ushey <kevinushey@gmail.com>
2014-10-05 12:21:02 -0700

    Hide R CMD check note re: 'attach'

--- R/rex-mode.R
+++ R/rex-mode.R
@@ -1,24 +1,25 @@
 
 
 #' Rex Mode
-#' 
+#'
 #' \code{rex_mode()} switch \pkg{rex} mode on and off. While within
 #' rex mode, functions used within the \code{\link{rex}} function are
 #' attached, and so one can get e.g. auto-completion within editors.
-#' 
+#'
 #' @export
 rex_mode <- function() {
-  
+
   ## Enter rex mode
   if (!.rex$mode) {
     .rex$mode <- TRUE
-    attach(.rex$env)
+    ## We know what we're doing, so hide the R CMD check note
+    eval(call("attach", call("$", as.name(".rex"), as.name("env"))))
   }
-  
+
   ## Exit rex mode
   else {
     .rex$mode <- FALSE
     detach(.rex$env)
   }
-  
+
 }

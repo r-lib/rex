d98401c868d918d4dc2b76e76b55a801407ed93b
Jim Hester <james.f.hester@gmail.com>
2014-10-06 12:41:54 -0400

    Split capture tests into separate file

---
+++ tests/testthat/test-capture.R
@@ -0,0 +1,65 @@
+context("capture")
+test_that("matches basic characters", {
+  x = "text"
+  re <- rex(capture(x))
+
+  expect_equal(re, regex("(text)"))
+
+  expect_true(grepl(re, x))
+
+  expect_equal(gsub(re, "\\1", x), x)
+
+  expect_equal(gsub(re, "replacement", x), "replacement")
+})
+
+test_that("escapes special characters", {
+  x = "^[x$"
+  re <- rex(capture(x))
+
+  expect_equal(re, regex("(\\^\\[x\\$)"))
+
+  expect_true(grepl(re, x))
+
+  expect_equal(gsub(re, "\\1", x), x)
+
+  expect_equal(gsub(re, "replacement", x), "replacement")
+})
+
+test_that("examples work", {
+ re <- rex(
+   # first quotation mark
+   capture(quote),
+
+   # match all non-matching quotation marks
+   zero_or_more(except(capture_group(1))),
+
+   # end quotation mark (matches first)
+   capture_group(1)
+ )
+
+ expect_equal(re, regex("(['\"])(?:[^\\g{1}])*\\g{1}"))
+
+ lapply(c("\"\"", "\"'\"", "\"arst\"", "''", "'arst'", "'\"'"),
+   function(x) {
+     expect_true(grepl(re, x, perl = TRUE), info=x)
+   }
+ )
+
+ lapply(c("'a", "'asr\""), function(x) {
+    expect_false(grepl(re, x, perl = TRUE), info = x)
+ })
+
+})
+
+context("named capture")
+test_that("examples work", {
+  re <- rex(
+    capture(name = "fruit", or("apple", "orange")),
+    "=",
+    capture_group("fruit")
+    )
+
+  expect_true(grepl(re, "apple=apple", perl=TRUE))
+  expect_true(grepl(re, "orange=orange", perl=TRUE))
+  expect_false(grepl(re, "apple=orange", perl=TRUE))
+})
--- tests/testthat/test-character_class.R
+++ tests/testthat/test-character_class.R
@@ -74,3 +74,35 @@ test_that("escapes correctly", {
   expect_true(grepl(re, "{", perl = TRUE))
 })
 
+context("character_class")
+test_that("examples are correct", {
+  # grey = gray
+  re <- rex("gr", one_of("a", "e"), "y")
+  expect_equal(grepl(re, c("grey", "gray")), c(TRUE, TRUE)) # TRUE TRUE
+
+  # Match non-vowels
+  re <- rex(none_of("a", "e", "i", "o", "u"))
+  # They can also be in the same string
+  re2 <- rex(none_of("aeiou"))
+  expect_identical(re, re2)
+  expect_equal(grepl(re, c("k", "l", "e")), c(TRUE, TRUE, FALSE)) # TRUE TRUE FALSE
+
+  # Match range
+  re <- rex(range("a", "e"))
+  expect_equal(grepl(re, c("b", "d", "f")), c(TRUE, TRUE, FALSE)) # TRUE TRUE FALSE
+
+  # Explicit creation (note you have to escape manually here)
+  re <- rex(character_class("abcd\\["))
+  expect_equal(grepl(re, c("a", "d", "[", "]")), c(TRUE, TRUE, TRUE, FALSE)) # TRUE TRUE TRUE FALSE
+})
+test_that("escapes special characters", {
+  re <- rex(exclude_range("[", "}"))
+
+  expect_equal(re, regex("[^\\[-}]"))
+
+  lapply(c("[", "}"), function(x) {
+    expect_false(grepl(re, x, perl = TRUE), info=x)
+  })
+
+  expect_true(grepl(re, "A", perl = TRUE))
+})

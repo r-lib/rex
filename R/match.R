28d133e3f9b01210873619d83add6a24375a768e
RobertZK <technoguyrob@gmail.com>
2015-03-09 11:53:59 -0500

    Slight improvements to documentation of re_substitutes

--- R/match.R
+++ R/match.R
@@ -16,7 +16,7 @@
 #' @seealso \code{\link{regexp}} Section "Perl-like Regular Expressions" for a
 #' discussion of the supported options
 #' @examples
-#' string = c("this is a", "test string")
+#' string <- c("this is a", "test string")
 #' re_matches(string, rex("test")) # FALSE FALSE
 #'
 #' # named capture
@@ -96,7 +96,7 @@ re_matches <- function(data, pattern, global = FALSE, options = NULL, locations 
   }
 }
 
-#' Substitution function
+#' Substitute regular expressions in a string with another string.
 #'
 #' @param data character vector to substitute
 #' @param pattern regular expression to match
@@ -107,9 +107,10 @@ re_matches <- function(data, pattern, global = FALSE, options = NULL, locations 
 #' @seealso \code{\link{regexp}} Section "Perl-like Regular Expressions" for a
 #' discussion of the supported options
 #' @examples
-#' string = c("this is a Test", "string")
+#' string <- c("this is a Test", "string")
 #' re_substitutes(string, "test", "not a test", options = "insensitive")
 #' re_substitutes(string, "i", "x", global = TRUE)
+#' re_substitutes(string, "(test)", "not a \\1", options = "insensitive")
 #' @export
 re_substitutes <- function(data, pattern, replacement, global = FALSE, options = NULL, ...) {
 

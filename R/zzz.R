9a907321edb84b47bed7d121ebbfc9d51726d741
Jim Hester <james.f.hester@gmail.com>
2015-11-13 08:21:57 -0500

    Only print startup message some of the time.

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 .onAttach <- function(lib, pkg) { # nolint
+  if (!interactive() || stats::runif(1) > 0.1) return()
+
   packageStartupMessage("Welcome to rex, the friendly regular expression helper!\n",
                         "Use 'rex_mode()' to toggle code completion for rex shortcuts and functions.")
 }

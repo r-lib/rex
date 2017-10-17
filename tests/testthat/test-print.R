ae7b037602511d4e5e3a967a97fe4bf502859453
Jim Hester <james.f.hester@gmail.com>
2017-10-17 09:47:10 -0400

    Remove test mocking globalVariables

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -6,9 +6,3 @@ test_that("print S3 method works properly", {
 
   expect_output(print(rex("x[")), "x\\\\\\[")
 })
-
-test_that("register_shortcuts calls utils::globalVariables", {
-  with_mock(`utils::globalVariables` = function(x, ...) x,
-    expect_equal(ls(.rex$env), register_shortcuts("rex"))
-  )
-})
